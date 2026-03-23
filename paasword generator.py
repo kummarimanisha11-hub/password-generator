a2b71b0ab76390cda30e5bfce89b65f46709b479
kummari Mannesha <kummarimanisha11@gmail.com>
2026-03-23 11:24:23 +0100

    Update paasword generator.py

--- paasword generator.py
+++ paasword generator.py
@@ -1,1 +1,20 @@
+import random
+import string
 
+# Function to generate password
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ""
+
+    for i in range(length):
+        password += random.choice(characters)
+
+    return password
+
+# User input
+length = int(input("Enter password length: "))
+
+# Generate password
+result = generate_password(length)
+
+print("Generated Password:", result)
